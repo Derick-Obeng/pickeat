c5382f0e507cb59d24ca324832a26c72a239fb17
Derrick Obeng <derickobeng023@gmail.com>
2025-05-09 03:03:17 +0000

    map

--- screens/Map.jsx
+++ screens/Map.jsx
@@ -1,6 +1,7 @@
-import { View, Text } from 'react-native'
+import { View, Text ,StyleSheet,TouchableOpacity, Keyboard} from 'react-native'
 import React from 'react'
 import Ionicons from "@expo/vector-icons/Ionicons";
+import { TouchableWithoutFeedback , TextInput} from 'react-native-gesture-handler'
 
 export default function Map({navigation}) {
   return (
@@ -12,6 +13,7 @@ export default function Map({navigation}) {
             <Ionicons name="arrow-back-outline" size={24} color="black" />
           </TouchableOpacity>
           <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+            <Text style={styles.title}>Set delivery address</Text>
             
           </TouchableWithoutFeedback>
     </View>
