2d78e7ee1d2d47358127afcf086304fcefa784fa
Derrick Obeng <derickobeng023@gmail.com>
2025-05-09 02:55:18 +0000

    maps

--- screens/Map.jsx
+++ screens/Map.jsx
@@ -1,10 +1,19 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import Ionicons from "@expo/vector-icons/Ionicons";
 
 export default function Map({navigation}) {
   return (
     <View>
-      <Text>Map</Text>
+     <TouchableOpacity
+            onPress={() => navigation.goBack()}
+            style={styles.backButton}
+          >
+            <Ionicons name="arrow-back-outline" size={24} color="black" />
+          </TouchableOpacity>
+          <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+            
+          </TouchableWithoutFeedback>
     </View>
   )
 }
