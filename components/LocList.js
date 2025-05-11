b804ae0fca2b7553a3576243b2bad713a7b16d4a
Derrick Obeng <derickobeng023@gmail.com>
2025-05-11 23:22:57 +0000

    locations

---
+++ components/LocList.js
@@ -0,0 +1,29 @@
+export const LocList = [
+  {
+    id: 1,
+    title: "Home",
+    address: "123 Main St, Cityville",
+    coordinates: {
+      latitude: 37.78825,
+      longitude: -122.4324,
+    },
+  },
+  {
+    id: 2,
+    title: "Work",
+    address: "456 Elm St, Townsville",
+    coordinates: {
+      latitude: 37.78825,
+      longitude: -122.4324,
+    },
+  },
+  {
+    id: 3,
+    title: "Gym",
+    address: "789 Oak St, Villageville",
+    coordinates: {
+      latitude: 37.78825,
+      longitude: -122.4324,
+    },
+  },
+];
